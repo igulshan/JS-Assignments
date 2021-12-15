5784b4887f25740346dcf8c904e333cab81f79a5
eric407 <ericmishra02@gmail.com>
2021-12-15 21:25:07 +0530

    todo list with API

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,21 +1,65 @@
-var c = 0;
+// var input= document.getElementsByTagName('input')[0];
+var place= document.getElementsByClassName('todos')[0];
+var complete= document.getElementsByTagName('button')[0];
+var notComplete= document.getElementsByTagName('button')[1];
+let todolist =[];
 
-var div = document.getElementsByClassName('todos')[0];
-var Btn = document.getElementsByTagName('button')[0];
-var Input = document.getElementsByTagName('input')[0];
+// add.addEventListener('click', function(){
+//     var p = document.createElement('p');
+//     p.textContent= input.value;
+//     p.addEventListener('click',function(){
+//         place.removeChild(p);
+//     })
+//     p.setAttribute('key',c)
+//     place.append(p);
+//     c++;
+//     input.value=''
+// })
 
-Btn.addEventListener('click',()=>{
-    var text = document.createElement('p');
-    text.setAttribute("key", c);
-    text.textContent = Input.value;
-    text.addEventListener("click",function(){
-        remove(this);
-    })
-    div.appendChild(text)
-    Input.value = ''
-    c += 1;
+fetch('https://jsonplaceholder.typicode.com/todos')
+  .then(response => response.json())
+  .then(json =>{
+    todolist = json.slice(0,10);
+    console.log(todolist);
+    todolist.forEach((items,index) => {
+      var item = document.createElement('p');
+      item.setAttribute('key',index);
+      item.innerHTML = items.title
+      place.append(item);
+      
+    });
+  })
+  .catch(e => console.log(e));
+
+
+
+complete.addEventListener('click',function(){
+  place.innerHTML='';
+  todolist.forEach((items,index) =>{
+    if (items.completed == true){
+      var item = document.createElement('p');
+      item.setAttribute('key',index);
+      item.innerHTML = items.title
+      place.append(item);
+
+    }
+  });
+
+
 })
 
-function remove(elem){
-    elem.remove();
-}
+notComplete.addEventListener('click',function(){
+  place.innerHTML='';
+  todolist.forEach((items,index) =>{
+    if (items.completed != true){
+      var item = document.createElement('p');
+      item.setAttribute('key',index);
+      item.innerHTML = items.title
+      place.append(item);
+
+    }
+  });
+
+
+})
+
